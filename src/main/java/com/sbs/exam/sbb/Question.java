0a235b07bf74a5562dd46e6251790ec3131e9a83
JangGunWook <zhsksrjsdnr@naver.com>
2024-10-24 00:36:28 +0900

    리포지터리 추가 후 테스트케이스

--- src/main/java/com/sbs/exam/sbb/Question.java
+++ src/main/java/com/sbs/exam/sbb/Question.java
@@ -1,11 +1,14 @@
 package com.sbs.exam.sbb;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Getter
+@Setter
 @Entity//아래 Question 크래스는 엔터티 클래스이다
 //아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
 
@@ -15,16 +18,17 @@ public class Question {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "title", length = 200) //VARCHAR 애의 너비를
-    private String title;
+    @Column(name = "subject", length = 200) //VARCHAR 애의 너비를
+    private String subject;
 
     @Column(length = 1000, nullable = false)   //oracle 에는 TEXT라는 데이터 타입이 없음
     private String content;
 
-    private LocalDateTime crateDate;
+    private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) //CascadeType.REMOVE : Question에 달려있는 Answer클래스의 answer까지 자동으로 삭제됨
     // 실제 테이블에는 생성되지 않음! DB컬럼에는 정보를 1개만 넣을 수 있기 때문이다
     private List<Answer> answerList;
+
 
 }
